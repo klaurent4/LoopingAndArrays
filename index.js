9fce22dc814edf9cbabc72b80f045e9a9c0f497c
Kerven <Kervenlaurenttsic@gmail.com>
2021-02-14 23:56:41 -0500

    initialize

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-var i
+var myArray = [1000]
 for (var i = 1000; i >= 0; i -= 2) {
-    myLoop.push(i);
+    myArray.push(i);
 }
 
-var u
-for (var u = 0; u <= 10000; u++) {
-    alert('A quarter of the way there!', u = 2500)
-    alert('Halfway there!', u = 5000)
-    alert('The loop is done!', u = 10000)
+var ourArray = [10000]
+for (var i = 0; i <= 10000; i++) {
+    ourArray.push(i);
+    alert('A quarter of the way there!', i = 2500)
+    alert('Halfway there!', i = 5000)
+    alert('The loop is done!', i = 10000)
 }
 
-var topFive = ["iCarly", "Great Pretender", "Code Lyoko", "One Piece", "Attack on Titan"]; {
-    console.log('My #1 favorite tv show is' [4])
-    console.log('My #2 favorite tv show is' [3])
-    console.log('My #3 favorite tv show is' [1])
-    console.log('My #4 favorite tv show is' [2])
-    console.log('My #5 favorite tv show is' [0])
+var topFive = ["iCarly", "Great Pretender", "Code Lyoko", "One Piece", "Attack on Titan"];
+    console.log("My #1 favorite tv show is" [4])
+    console.log("My #2 favorite tv show is" [3])
+    console.log("My #3 favorite tv show is" [1])
+    console.log("My #4 favorite tv show is" [2])
+    console.log("My #5 favorite tv show is" [0])
 }
