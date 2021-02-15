3e2455af6354b783a7c83abe01c8dc967f566988
Kerven <Kervenlaurenttsic@gmail.com>
2021-02-14 23:26:39 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var i
+for (var i = 1000; i >= 0; i -= 2) {
+    myLoop.push(i);
+}
+
+var u
+for (var u = 0; u <= 10000; u++) {
+    alert('A quarter of the way there!', u = 2500)
+    alert('Halfway there!', u = 5000)
+    alert('The loop is done!', u = 10000)
+}
+
+var topFive = ["iCarly", "Great Pretender", "Code Lyoko", "One Piece", "Attack on Titan"]; {
+    console.log('My #1 favorite tv show is' [4])
+    console.log('My #2 favorite tv show is' [3])
+    console.log('My #3 favorite tv show is' [1])
+    console.log('My #4 favorite tv show is' [2])
+    console.log('My #5 favorite tv show is' [0])
+}
