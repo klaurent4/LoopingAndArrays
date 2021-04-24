739a7c2da8adcb2e8a35b830aea524e4bfda029f
Kerven <Kervenlaurenttsic@gmail.com>
2021-04-24 14:40:28 -0400

    second commit

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 var myArray = [1000]
 for (var i = 1000; i >= 0; i -= 2) {
     myArray.push(i);
+    if (i % 2 == 0) {
+        console.log(i)
+    }
 }
+
 
 var ourArray = [10000]
 for (var i = 0; i <= 10000; i++) {
     ourArray.push(i);
-    alert('A quarter of the way there!', i = 2500)
-    alert('Halfway there!', i = 5000)
-    alert('The loop is done!', i = 10000)
+    if (i = 2500) {
+    alert('A quarter of the way there!')
+    }
+    if (i = 5000) {
+    alert('Halfway there!')
+    }
+    if (i = 10000) {
+    alert('The loop is done!')
+    }
 }
 
 var topFive = ["iCarly", "Great Pretender", "Code Lyoko", "One Piece", "Attack on Titan"];
-    console.log("My #1 favorite tv show is" [4])
-    console.log("My #2 favorite tv show is" [3])
-    console.log("My #3 favorite tv show is" [1])
-    console.log("My #4 favorite tv show is" [2])
-    console.log("My #5 favorite tv show is" [0])
+    console.log("My #1 favorite tv show is ", topFive[4])
+    console.log("My #2 favorite tv show is ", topFive[3])
+    console.log("My #3 favorite tv show is ", topFive[1])
+    console.log("My #4 favorite tv show is ", topFive[2])
+    console.log("My #5 favorite tv show is ", topFive[0])
